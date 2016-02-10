945570322e17d702404ca65b7a18a6ee4f04d171
samizhang <sami.zhang90@gmail.com>
2016-02-10 02:51:07 +0000

    files updated

--- week-4/calculate-grade/my_solution.rb
+++ week-4/calculate-grade/my_solution.rb
@@ -4,3 +4,21 @@
 
 
 # Your Solution Below
+puts "Enter grade"
+grade = gets.to_i
+
+def get_grade(grade)
+    if grade >= 90
+        puts "A"
+    elsif grade >= 80
+        puts "B"
+    elsif grade >= 70
+        puts "C"
+    elsif grade >= 60
+        puts "D"
+    else 
+        puts "F"
+    end
+end    
+    
+puts get_grade(grade)
