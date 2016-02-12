5406db39b53f553fe6c1d1fcb81075b6c485bf51
samizhang <sami.zhang90@gmail.com>
2016-02-12 18:38:46 +0000

    4.7 files added

---
+++ week-4/factorial/my_solution.rb
@@ -0,0 +1,18 @@
+# Factorial
+
+# I worked on this challenge with Dan Shapiro.
+
+
+# Your Solution Below
+def factorial(n)
+    if n == 0 
+      p 1
+    elsif n < 0
+      puts 'Cannot factorial a negative number.'  
+    else 
+      p n * factorial(n-1)
+    end
+    
+end
+
+factorial(-9)
