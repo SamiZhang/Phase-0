9db7d17f514a66c890062c0bdae1bc4f370c3ca5
samizhang <sami.zhang90@gmail.com>
2016-02-17 02:10:21 +0000

    5.5 started

---
+++ week-5/nums-commas/my_solution.rb
@@ -0,0 +1,44 @@
+# Numbers to Commas Solo Challenge
+
+# I spent [] hours on this challenge.
+
+# Complete each step below according to the challenge directions and
+# include it in this file. Also make sure everything that isn't code
+# is commented in the file.
+
+# 0. Pseudocode
+# What is the input? 
+    # Input will be a positive integer as an argument
+    
+# What is the output? (i.e. What should the code return?)
+    # Output will be a positive integer with comma(s) separating it
+    
+# What are the steps needed to solve the problem?
+    # Create a method named separate_comma with an argument(num)
+        # Turn argument(num) into string
+        # Counts the characters of the argument(num)
+        # Split the argument(number) with comma
+            # No comma IF length is less than 4
+            # Put comma IF length is more than 3
+                # Place comma after a count of 3 characters 
+    # End method
+
+
+# 1. Initial Solution
+def separate_comma(num)
+    s = num.to_s
+    l = s.length
+    if l < 4
+        p num
+    elsif l > 3
+        
+    end    
+end  
+separate_comma(90)
+
+# 2. Refactored Solution
+
+
+
+
+# 3. Reflection
