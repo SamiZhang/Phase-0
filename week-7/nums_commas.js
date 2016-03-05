40cb653dd56f118246ff627216f50cefebd66d7a
samizhang <sami.zhang90@gmail.com>
2016-03-05 22:53:16 +0000

    pair sessions done

--- week-7/nums_commas.js
+++ week-7/nums_commas.js
@@ -45,8 +45,14 @@ console.log(num.toLocaleString());
 // Reflection
 /*
 What was it like to approach the problem from the perspective of JavaScript? Did you approach the problem differently?
-We approach the problem basically the same we would approach 
+We approach this problem basically the same as Ruby for this problem, except that we translated the code into Javascript.
+
 What did you learn about iterating over arrays in JavaScript?
+Iterating over arrays in JavaScript is similar to to the idea of iteratin arrays in Ruby, but the coding is different. For example, to interate in Ruby we can use IF/ELSE and we type the condition next to IF/ELSE. As for JavaScript, we can also use IF/ELSE but we type the condition inside a parentheses after IF/ELSE.
+
 What was different about solving this problem in JavaScript?
+One thing I noticed was that we created more varialbes to solve this problem compare with Ruby. We tried to squeeze some of the methods into one variable but it kept returning "undefine" or "no such method".
+
 What built-in methods did you find to incorporate in your refactored solution?
+My partner found this method called tolocaleString and it automatically placed the commas into any numbers. We both still not sure how exactly this method was able to do it and it seems like this method can be used on many different conditions. It's a very useful and powerful method!
 */
