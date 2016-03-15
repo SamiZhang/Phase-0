87d3c93512a54390c3f37edbf457142c10f74951
samizhang <sami.zhang90@gmail.com>
2016-03-15 18:13:02 +0000

    9.3 done

---
+++ week-9/home_page.js
@@ -0,0 +1,36 @@
+// DOM Manipulation Challenge
+
+
+// I worked on this challenge with Kevin Fowle.
+
+
+// Add your JavaScript calls to this page:
+
+// Release 1:
+document.getElementById("release-0").className = "done";
+
+
+
+// Release 2:
+document.getElementById("release-1").style.display="none";
+
+
+
+// Release 3:
+document.getElementById("change").innerHTML = "I completed release 2.";
+
+
+
+// Release 4:
+document.getElementById("release-3").style.background = "#955251"
+
+
+
+// Release 5:
+document.getElementById("release-3").style.fontSize = "2em"
+
+
+
+// Release 6:
+var tmpl = document.getElementById('hidden');
+document.body.appendChild(tmpl.content.cloneNode(true));
